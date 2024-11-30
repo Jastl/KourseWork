9e0ceb42383c8cde077b3f2c9402973336ddf09e
Jastl <112689025+Jastl@users.noreply.github.com>
2024-11-30 14:20:01 +0200

    Create constructor in Patient

--- RegistryManagamant/Patient.cs
+++ RegistryManagamant/Patient.cs
@@ -6,5 +6,13 @@
         public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string PhoneNumber { get; set; }
+
+        public Patient(string firstName, string lastName, DateTime dateOfDirth, string phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfDirth;
+            PhoneNumber = phoneNumber;
+        }
     }
 }
